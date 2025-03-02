0586944d3b67e1b9c5ddcf6993d13d073d55b72c
Edson Rosales <edsonrosaless@live.com>
2025-03-01 20:13:10 -0600

    fix console warning.

--- src/tools.cpp
+++ src/tools.cpp
@@ -379,7 +379,7 @@ std::string formatDate(time_t time)
 		return {};
 	}
 
-	char buffer[20];
+	char buffer[75];
 	int res = sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", tms->tm_mday, tms->tm_mon + 1, tms->tm_year + 1900, tms->tm_hour, tms->tm_min, tms->tm_sec);
 	if (res < 0) {
 		return {};
