00a560f030d014106748d9e7c64cf82e1133ee9d
Edson Rosales <edsonrosaless@live.com>
2025-03-01 23:36:08 -0600

    Update spawn.cpp

--- src/spawn.cpp
+++ src/spawn.cpp
@@ -333,10 +333,10 @@ bool Spawn::spawnMonster(uint32_t spawnId, MonsterType* mType, const Position& p
 
 	if (startup) {
 		//No need to send out events to the surrounding since there is no one out there to listen!
-		/*if (!g_game.internalPlaceCreature(monster_ptr.get(), pos, true)) {
+		if (!g_game.internalPlaceCreature(monster_ptr.get(), pos, false, true)) {
 			std::cout << "[Warning - Spawns::startup] Couldn't spawn monster \"" << monster_ptr->getName() << "\" on position: " << pos << '.' << std::endl;
 			return false;
-		}*/
+		}
 	} else {
 		if (!g_game.placeCreature(monster_ptr.get(), pos, false, true)) {
 			return false;
